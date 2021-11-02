ed6585ec3fcdc9c45624a522c7eccff5b866359f
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-02 13:47:02 +0530

    Area and Perimeter of a Rectangle

---
+++ P9_AreaRectangle.c
@@ -0,0 +1,13 @@
+//Area and Perimeter of a Rectangle
+#include<stdio.h>
+void main()
+{
+    float l=10,b=20;
+    float a,p;
+    a=l*b;
+    p=2*(l+b);
+    printf("Area=%f\n",a);
+    printf("Perimeter=%f",p);
+    getch();
+
+}
