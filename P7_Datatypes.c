b5e4d34e5bccb71b94a580b83e6d029d2f085ffb
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-02 13:31:32 +0530

    Use of Datatypes

---
+++ P7_Datatypes.c
@@ -0,0 +1,15 @@
+//Use of Datatypes
+#include<stdio.h>
+void main()
+{
+    int a=10;
+    float b=25.34;
+    char c='A';
+    double d=542879.21456;
+    printf("%d\n",a);
+    printf("%f\n",b);
+    printf("%c\n",c);
+    printf("%lf",d);
+    getch();
+
+}
