a5eb8b09e0650ec8f9bd5a86d46638f5cf9a82ed
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-02 13:30:30 +0530

    Use of Escape Sequence

---
+++ P5_EscapeSequence.c
@@ -0,0 +1,14 @@
+//Use of Escape Sequence
+#include<stdio.h>
+int main()
+{
+
+    printf("Roll\tName\t\tContact_number\n");
+    printf("1\tAyan Das\t9874563210\n");
+    printf("2\tAyan Das\t9874563210\n");
+    printf("3\tAyan Das\t9874563210");
+
+
+    getch();
+
+}
