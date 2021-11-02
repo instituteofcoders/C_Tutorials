722d913fd6e303b2bdd8869c59b221d4a10e1b5c
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-02 13:30:58 +0530

    Use of Keyword, Identifier

---
+++ P6_KeywordIdentifier.c
@@ -0,0 +1,12 @@
+//Use of Keyword, Identifier
+#include<stdio.h>
+void main()
+{
+
+    int n_1=500;
+    float b=25.34;
+    printf("Hello World");//Library function
+    int sum(a,b);//User defined function
+    getch();
+
+}
