08929711ee438dd3f8a3548dd9ebbfd7df6bea00
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-02 13:46:13 +0530

    Use of sizeof()

---
+++ P8_Sizeof.c
@@ -0,0 +1,15 @@
+//Use of sizeof()
+#include<stdio.h>
+void main()
+{
+    int a=10;
+    float b=25.34;
+    char c='A';
+    double d=542879.21456;
+    printf("Size of int=%d\n",sizeof(a));
+    printf("Size of float=%d\n",sizeof(b));
+    printf("Size of char=%d\n",sizeof(c));
+    printf("Size of double=%d",sizeof(d));
+    getch();
+
+}
