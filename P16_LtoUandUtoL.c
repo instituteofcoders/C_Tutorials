b2ec197b3e93cdb59287deddb9fa9c228c4446ef
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-03 15:00:52 +0530

    Use of toupper() and tolower()

---
+++ P16_LtoUandUtoL.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char l,u;
+printf("Upper case letter=");
+scanf("%c",&u);
+l=tolower(u);//Predefined formula. Here tolower() is a library function
+printf("Lower case letter=%c",l);
+
+
+
+    getch();
+}
