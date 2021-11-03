1d4f2c33158b75876e79605d132106e405dcd6ed
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-03 15:00:22 +0530

    Lower case letter to Upper case letter & vice-versa

---
+++ P15_LtoUandUtoL.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char u;
+//Upper to Lower
+printf("Enter a upper case letter:");
+scanf("%c",&u);
+printf("\nLower case letter is=%c",u+32);
+
+
+
+    getch();
+}
